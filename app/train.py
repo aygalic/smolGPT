3aaa72ddeb2abfd1b226757ef70f26b9acba6d30
aygalic <aygalic@hotmail.fr>
2024-12-01 14:40:02 +0100

    chore: tokenization + train_test split

--- app/train.py
+++ app/train.py
@@ -1,3 +1,4 @@
+import torch
 
 with open("./data/corpus.txt", "r", encoding="utf-8") as f:
     text = f.read()
@@ -9,7 +10,7 @@ print("".join(chars))
 print(f"Vacabulary size : {vacab_size=}")
 
 
-# encoder/decoder
+# tokenizer
 stoi = {s:i for (i,s) in enumerate(chars)}
 itos = {i:s for (i,s) in enumerate(chars)}
 encode = lambda s : [stoi[c] for c in s]
@@ -17,3 +18,14 @@ decode = lambda l : [itos[i] for i in l]
 
 print(encode("test"))
 print(decode(encode("test")))
+
+# tokenize data
+data = torch.tensor(encode(text), dtype = torch.long )
+print(data.shape, data.dtype)
+print(data[:10])
+
+# train/test split:
+n = int(len(data)*0.9)
+train_data = data[:n]
+test_data = data[n:]
+
