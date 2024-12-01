7ef60de274b61d2b82c2adbbc033272f90c60ddb
aygalic <aygalic@hotmail.fr>
2024-12-01 15:47:42 +0100

    feat: simple bigram model

--- app/train.py
+++ app/train.py
@@ -1,4 +1,7 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
 
 with open("./data/corpus.txt", "r", encoding="utf-8") as f:
     text = f.read()
@@ -65,3 +68,41 @@ for b in range(batchs_size):
         context = xb[b, :t+1]
         target = yb[b,t]
         print(f"When {context=}, {target=}")
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets = None):
+        logits = self.token_embedding_table(idx) # (B, T, C)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx shape is (B,T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get preds:
+            logits, loss = self(idx)
+            # focus on last time step:
+            logits = logits[:, -1, :] # (B, C)
+            # softmax for probs
+            probs = F.softmax(logits, dim = -1) # (B, C)
+            # sample from dist:
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append the sampled token to the running sequence 
+            idx = torch.cat((idx, idx_next), dim = 1) # (B, T+1)
+        return idx
+    
+m = BigramLanguageModel(vacab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
+idx = torch.zeros((1,1), dtype=torch.long)
+print(decode(m.generate(idx, max_new_tokens=100)[0].tolist()))
