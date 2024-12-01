957309fc2d5bb5d3f3bbf45a579425dc2451adb8
aygalic <aygalic@hotmail.fr>
2024-12-01 15:02:33 +0100

    feat: batch generation

--- app/train.py
+++ app/train.py
@@ -29,3 +29,39 @@ n = int(len(data)*0.9)
 train_data = data[:n]
 test_data = data[n:]
 
+# context_length / block_size
+context_length = 8
+print(f"first block is {train_data[:context_length+1]}")
+
+for i in range(context_length+1):
+    print(f"context is {train_data[:i]}, target is {train_data[i]}")
+
+
+torch.manual_seed(123)
+batchs_size = 4
+block_size = 8
+
+def get_batch(split):
+    data = train_data if split == "train" else test_data
+    ix = torch.randint(len(data) - block_size, (batchs_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+
+xb, yb = get_batch("train")
+print("inputs:")
+print(xb.shape)
+print(xb)
+
+print("targets:")
+print(yb.shape)
+print(yb)
+
+print("------")
+
+for b in range(batchs_size):
+    for t in range(block_size):
+        context = xb[b, :t+1]
+        target = yb[b,t]
+        print(f"When {context=}, {target=}")
