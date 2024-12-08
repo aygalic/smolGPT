83932edb579fe99233576ed72c022e5838350f6c
aygalic <aygalic@hotmail.fr>
2024-12-08 22:05:23 +0100

    chore: cleanup

--- app/train_transformer.py
+++ app/train_transformer.py
@@ -1,4 +1,5 @@
 """Draft for transformers"""
+
 from lightning import Trainer
 import torch
 from smolgpt.model.transformer import Transformer
@@ -8,20 +9,18 @@ DEVICE = "mps"
 torch.manual_seed(123)
 block_size = 256
 batch_size = 64
-learning_date = 3e-4
+
 max_iter = 5000
-eval_iter= 200
-eval_interval = 300
-n_heads = 2
-n_embed = n_heads*32
+
+
+n_heads = 4
+n_embed = n_heads * 32
 n_layer = 2
 dropout = 0.2
 
 
 data_module = TinyShakespeareData(
-    path_to_dir="./data/corpus.txt",
-    batch_size=64,
-    block_size=256
+    path_to_dir="./data/corpus.txt", batch_size=batch_size, block_size=block_size
 )
 data_module.setup()
 # Access vocab size for model initialization
@@ -33,7 +32,7 @@ trainer = Trainer(accelerator="mps", max_steps=max_iter)
 trainer.fit(model, data_module)
 
 m = model.to(DEVICE)
-idx = torch.zeros((1,1), dtype=torch.long, device=DEVICE)
-context = torch.zeros((1,1), dtype=torch.long, device=DEVICE)
+idx = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
+context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
 output = data_module.decode(m.generate(idx, max_new_tokens=1000)[0].tolist())
 print("".join(output))
