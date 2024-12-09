f3d437843a786c84d5277b8ea6c2f216177b9cfb
aygalic <aygalic@hotmail.fr>
2024-12-09 18:09:34 +0100

    chore: basic utf8 tokenization

--- app/tokenizer.py
+++ app/tokenizer.py
@@ -1,2 +1,32 @@
 """draft for implementing tokenization"""
 
+text = """the full recipe that defines how your nn.Modules interact.
+
+    The training_step defines how the nn.Modules interact together.
+
+    In the configure_optimizers define the optimizer(s) for your models.
+
+class LitAutoEncoder(L.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder#😎"""
+
+tokens = text.encode("utf-8")
+tokens = list(map(int, tokens))
+print(text)
+print("-----")
+print(len(text))
+print("-----")
+
+
+print(tokens)
+print("-----")
+print(len(tokens))
+print("-----")
