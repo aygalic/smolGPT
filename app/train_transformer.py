509a757d48607067e8b7e3b62f7abc31d9a9a617
aygalic <aygalic@hotmail.fr>
2024-12-14 22:35:23 +0100

    feat: model saving

--- app/train_transformer.py
+++ app/train_transformer.py
@@ -1,26 +1,29 @@
-"""Draft for transformers"""
+"""Train transformer with checkpointing"""
 
-from lightning import Trainer
+import json
+
 import torch
-from smolgpt.model.transformer import Transformer
+from lightning import Trainer
+from lightning.pytorch.callbacks import ModelCheckpoint
+
 from smolgpt.data.data_module import TinyShakespeareData
+from smolgpt.model.transformer import Transformer
 from smolgpt.tokenizer.bpe_tokenizer import BPETokenizer
 
 DEVICE = "mps"
 torch.manual_seed(123)
 block_size = 256
 batch_size = 64
-
-max_iter = 5000
-
+max_iter = 50
 
 n_heads = 4
 n_embed = n_heads * 32
 n_layer = 2
 dropout = 0.2
 
-tokenizer = BPETokenizer.load("tokenizer/vocab/")
 
+# Initialize tokenizer and data module
+tokenizer = BPETokenizer.load("tokenizer/vocab/")
 data_module = TinyShakespeareData(
     path_to_dir="./data/corpus.txt",
     batch_size=batch_size,
@@ -28,16 +31,39 @@ data_module = TinyShakespeareData(
     tokenizer=tokenizer,
 )
 data_module.setup()
-# Access vocab size for model initialization
 vocab_size = data_module.tokenizer.vocab_size
 
+# Initialize model
 model = Transformer(vocab_size, n_embed, block_size, n_heads, n_layer, dropout)
 
-trainer = Trainer(accelerator="mps", max_steps=max_iter)
+
+# Define checkpoint callback
+checkpoint_callback = ModelCheckpoint(
+    dirpath="checkpoints",
+    filename="transformer-{step}",
+    save_top_k=3,
+    verbose=True,
+    monitor="train_loss",
+    mode="min",
+)
+
+# Initialize trainer with checkpoint callback
+trainer = Trainer(
+    accelerator="mps", max_steps=max_iter, callbacks=[checkpoint_callback]
+)
+
+# Train model
 trainer.fit(model, data_module)
 
-m = model.to(DEVICE)
-idx = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
-context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
-output = data_module.tokenizer.decode(m.generate(idx, max_new_tokens=1000)[0].tolist())
-print("".join(output))
+# Save the tokenizer configuration along with the model
+model_info = {
+    "block_size": block_size,
+    "n_heads": n_heads,
+    "n_embed": n_embed,
+    "n_layer": n_layer,
+    "dropout": dropout,
+    "vocab_size": vocab_size,
+}
+
+with open("checkpoints/model_config.json", "w") as f:
+    json.dump(model_info, f)
