aefdde9ed1c2e578eddf1ed56bb625108d112957
aygalic <aygalic@hotmail.fr>
2024-12-17 12:25:10 +0100

    feat: auto detect last checkpoint for predictions

--- smolgpt/utils/custom_callback.py
+++ smolgpt/utils/custom_callback.py
@@ -1,4 +1,6 @@
 from lightning.pytorch.callbacks import Callback
+import glob
+import os
 
 class PredictionOutputCallback(Callback):
     def __init__(self):
@@ -26,4 +28,22 @@ class PredictionOutputCallback(Callback):
         self.outputs = []
 
 
+class AutoLoadLastCheckpointCallback(Callback):
+    def __init__(self, tokenizer_type: str = None):
+        if tokenizer_type is not None:
+            assert tokenizer_type in ["ASCII", "BPE"]
+        self.tokenizer_type = tokenizer_type
 
+
+    def on_predict_start(self, trainer, pl_module):
+        if not trainer.ckpt_path:  # If no checkpoint was explicitly provided
+            checkpoints_dir = "checkpoints"
+            checkpoint_files = glob.glob(os.path.join(checkpoints_dir, "*.ckpt"))
+            if self.tokenizer_type is not None:
+                checkpoint_files = [f for f in checkpoint_files if self.tokenizer_type in f]
+            if checkpoint_files:
+                latest_checkpoint = max(checkpoint_files, key=os.path.getmtime)
+                trainer.ckpt_path = latest_checkpoint
+                print(f"Automatically loading checkpoint: {latest_checkpoint}")
+            else:
+                print("No checkpoint were found!!")
