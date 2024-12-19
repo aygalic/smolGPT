71034f6b9665c5512cffbdd385da84a77ba9cb7d
aygalic <aygalic@hotmail.fr>
2024-12-19 15:36:57 +0100

    chore: declutter cli

--- app/train_transformer.py
+++ app/train_transformer.py
@@ -1,11 +1,9 @@
 from lightning.pytorch.cli import LightningCLI
-from smolgpt.data.data_module import TinyShakespeareData
 from smolgpt.model.transformer import Transformer
 
 def cli_main():
     cli = LightningCLI(
         model_class=Transformer,
-        datamodule_class=TinyShakespeareData,
         save_config_callback=None,
         parser_kwargs={"parser_mode": "yaml"}
     )
