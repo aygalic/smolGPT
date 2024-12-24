d812fe269b7f8103493ff4d06a6192a0a8eb1fe8
aygalic <aygalic@hotmail.fr>
2024-12-24 18:40:03 +0100

    doc: docstring

--- app/train_transformer.py
+++ app/train_transformer.py
@@ -1,12 +1,18 @@
+"""Main script to run training and inference through Lightning CLI"""
+
 from lightning.pytorch.cli import LightningCLI
+
 from smolgpt.model.transformer import Transformer
 
+
 def cli_main():
+    """Handle CLI"""
     cli = LightningCLI(
         model_class=Transformer,
         save_config_callback=None,
-        parser_kwargs={"parser_mode": "yaml"}
+        parser_kwargs={"parser_mode": "yaml"},
     )
+
 
 if __name__ == "__main__":
     cli_main()
