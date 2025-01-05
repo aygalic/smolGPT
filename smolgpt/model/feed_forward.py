e2104f3a55aad165f7a727f1d32dd86415527160
aygalic <aygalic@hotmail.fr>
2025-01-05 20:35:27 +0100

    doc: docstring + typing

--- smolgpt/model/feed_forward.py
+++ smolgpt/model/feed_forward.py
@@ -1,7 +1,17 @@
+"""Feed forward module part of the transformer architecture."""
 import torch.nn as nn
 
 class FeedForward(nn.Module):
-    def __init__(self, n_embed, dropout):
+    """Simple feed forward network part of the transformer architecture
+
+    Parameters
+    ----------
+    n_embed : int
+        Model latent dimension
+    dropout : float
+        Dropout rate
+    """
+    def __init__(self, n_embed: int, dropout: float):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_embed, n_embed*4),
