74bb763366f1d9e90d1c17363d70bd8408e95c0d
aygalic <aygalic@hotmail.fr>
2025-01-09 18:53:59 +0100

    chore: init doesn't need a base file doc: typing

--- smolgpt/tokenizer/ascii_tokenizer.py
+++ smolgpt/tokenizer/ascii_tokenizer.py
@@ -1,22 +1,22 @@
 from smolgpt.tokenizer.tokenizer import TokenizerABC
 
 class ASCIITokenizer(TokenizerABC):
+    """ASCII tokenizer based on tiny shakespeare data"""
     def __init__(self):
-        #FIXME : self init using tiny shakespeer, this should NOT be a thing
-        path_to_dir="./data/corpus.txt"
-        with open(path_to_dir, "r", encoding="utf-8") as f:
-            text = f.read()
+        text = "!$&',-.3:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        chars = list(set(text))
+        chars += [" ", "\n"]
+        chars = sorted(chars)
 
-        chars = sorted(list(set(text)))
         self.vocab_size = len(chars)
         self.stoi = {s: i for i, s in enumerate(chars)}
         self.itos = {i: s for i, s in enumerate(chars)}
 
 
-    def encode(self, x):
+    def encode(self, x: str) -> list[int]:
         """Convert string to list of integers"""
         return [self.stoi[c] for c in x]
     
-    def decode(self, x):
+    def decode(self, x: list[int]) -> str:
         """Convert list of integers back to string"""
         return [self.itos[i] for i in x]
